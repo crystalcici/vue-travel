11ac76dea1c7ee0f4519222a18b5c6ddb67fd9ce
master chen <517455797@qq.com>
2020-02-26 18:45:47 +0800

    css-header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    hello world
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 <style>
