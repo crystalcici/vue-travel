6331834c06785ff6ce64bcf2e679eec6f7a43d7b
master chen <517455797@qq.com>
2020-03-02 00:14:35 +0800

    change

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,9 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -23,7 +26,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display : flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -46,6 +49,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .24rem
