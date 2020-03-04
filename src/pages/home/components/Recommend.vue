ccb83f3a781e921cf805b71ae4d0174f273d0673
master chen <517455797@qq.com>
2020-03-04 17:23:54 +0800

    change

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <div class="title">
-    热销推荐
-    </div>
+    <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
-        <img class="item-img" :src="item.imgUrl"/>
+      <router-link
+        tag="li"
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
+        <img class="item-img" :src="item.imgUrl" />
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
-          <p calss="item-desc">{{item.desc}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
@@ -26,37 +30,37 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  @import "~styles/mixins.styl"
+  @import '~styles/mixins.styl'
   .title
     margin-top: .2rem
     line-height: .8rem
     background: #eee
     text-indent: .2rem
-   .item
-      display: flex
-      height: 1.9rem
-      overflow: hidden
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
     .item-img
       width: 1.7rem
       height: 1.7rem
-      padding: 0.1rem
+      padding: .1rem
     .item-info
       flex: 1
       padding: .1rem
       min-width: 0
       .item-title
-        ellipse()
         line-height: .54rem
         font-size: .32rem
+        ellipsis()
       .item-desc
-        ellipse()
         line-height: .4rem
         color: #ccc
+        ellipsis()
       .item-button
         line-height: .44rem
-        background: #ff9300
-        padding: 0 .1rem
-        border-radius: .06rem
         margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
         color: #fff
 </style>
