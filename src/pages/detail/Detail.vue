99741aaa148504a5205f0c32191ec84965fbab8b
crystalcici <517455797@qq.com>
2020-03-09 20:45:38 +0800

    change

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,40 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三晚联票'
+        }, {
+          title: '成人五晚联票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '学生票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -68,6 +68,7 @@ export default {
       color: #fff
       font-size: .4rem
   .header-fixed
+    z-index: 2
     position: fixed
     top: 0
     right: 0
