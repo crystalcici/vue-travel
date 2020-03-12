a650755e1ee1b45d688d3d73018c4a315fca1fa8
crystalcici <517455797@qq.com>
2020-03-12 20:47:10 +0800

    change

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -61,23 +61,23 @@ export default {
     .banner-img
       width: 100%
     .banner-info
-      position: absolute
       display: flex
-      left: .2rem
+      position: absolute
+      left: 0
       right: 0
       bottom: 0
       line-height: .6rem
-      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
       color: #fff
-      .banner-title
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
         flex: 1
         font-size: .32rem
         padding: 0 .2rem
       .banner-number
-        line-height: .32rem
         height: .32rem
+        line-height: .32rem
         margin-top: .14rem
-        padding: 0 1rem
+        padding: 0 .4rem
         border-radius: .2rem
         background: rgba(0, 0, 0, .8)
         font-size: .24rem
